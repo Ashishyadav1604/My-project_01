458d58d3b9c976a3c8d615f72420cafeabe99243
Ashishyadav1604 <aashishyadav1604@gmail.com>
2026-01-19 21:35:49 -1200

    first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+// dark mode
+
+let darkModeEnabled = localStorage.getItem('darkmodeEnabled') === 'true';
+
+const enableDarkMode = () => {
+    darkModeEnabled = true;
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('darkModeEnabled', 'true');
+}
+const disableDarkMode = () => {
+    darkModeEnabled = false;
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('darkModeEnabled', 'false');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    darkModeButton.checked = darkModeEnabled;
+    document.body.classList.toggle('dark-mode', darkModeEnabled)
+})
+
+const darkModeButton = document.getElementById('darkModeButton');
+
+darkModeButton.addEventListener('change', () => {
+    if (darkModeButton.checked) {
+        enableDarkMode()
+    } else {
+        disableDarkMode()
+    }
+})
+
+//Menu
+
+const hamburger = document.querySelector('.hamburger');
+const navlist = document.querySelector('nav ul')
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+    navlist.classList.toggle('active')
+
+})
+
+// multiple text
+
+const typed = new Typed('.multiple',{
+    strings: ['Front-End Developer','Web Designer','Youtuber'],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true
+});
+
+// header shadow on scroll effect
+
+const header =document.querySelector('header');
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY){
+           header.style.boxShadow='0 2px 4px rgba(0, 0, 0, 0.1)';
+    }else{
+        header.style.boxShadow='none'
+    }
+})
+
+// scroll section active link
+
+// let sections =document.querySelectorAll('section')
+// let navlinks=document.querySelectorAll('header nav a')
+
+// window.onscroll=()=>{
+//     sections.forEach(sec =>{
+//         let top = window.scrollY;
+//         let offset =sec.offsetTop-150;
+//         let height =sec.offsetHeight;
+//         let id =sec.getAttribute('id');
+
+//         if(top >=offset && top < offset + height){
+//             navlinks.forEach(link =>{
+//               link.classList.remove('active');  
+//             });
+//             document.querySelector('header nav ul li a[href="#' +id+' "]').classList.add('active ')
+//         }
+//     })
+// }
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.addEventListener('scroll', () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach(link => {
+        link.classList.remove('active');
+      });
+
+      document
+        .querySelector('header nav a[href="#' + id + '"]')
+        .classList.add('active');
+    }
+  });
+});
+
+navlinks.forEach(link => {
+    link.addEventListener('click',() =>{
+        hamburger.classList.remove('active');
+        navlist.classList.remove('active');
+    });
+});
